d1a255c2613aa6faf2b95ac311a5cf05e2feaecb
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-09 22:26:48 +0530

    added object oriented problems

---
+++ Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Inventory_Management
+{
+    internal class Player
+    {
+        public static Queue<Player> players;
+        public String player;
+        public string[] Cards = new String[9];
+        public override string ToString()
+        {
+            string st = "";
+            st += player + "\n";
+            for (int i = 0; i < Cards.Length; i++)
+            {
+                st += Cards[i] + " , ";
+            }
+            return st;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Problems_day_11_and_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
