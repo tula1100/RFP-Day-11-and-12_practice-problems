364078aa393378c1c86af46f114b51bdc65f6e8e
Tulasi <93372595+tula1100@users.noreply.github.com>
2022-03-09 22:42:46 +0530

    Update DeckOfCardQueue.cs

--- DeckOfCardQueue.cs
+++ DeckOfCardQueue.cs
@@ -8,5 +8,35 @@ namespace Inventory_Problems_day_11_and_12
 {
     internal class DeckOfCardQueue
     {
+          public DeckCardQueue()
+        {
+            ///Create DeckOfCard Class Object 
+            DeckOfCard DeckOfCardObject = new DeckOfCard();
+            string[] Deck = DeckOfCardObject.Play();//By using DeckOfCard refence call Play() method Present inside the DeckOfCard class
+            string[] ShuffleCardDeck = DeckOfCardObject.CardShuffle(Deck);//Call CardShuffle method and it returns Shuffle card string array
+            int Player = 1, k = 0;
+
+            //create Queue object
+            if (players == null)
+            {
+                players = new Queue<Player>();
+            }
+            //Add Player class object and object content into the Queue
+            for (int i = 1; i <= 4; i++)
+            {
+                Player p = new Player();
+                p.player = "Player " + (Player++);
+                for (int j = 0; j < 9; j++)
+                {
+                    p.Cards[j] = ShuffleCardDeck[k++]; //Distrubute 9 card for each player 
+                }
+                players.Enqueue(p);//Add Player object into the  Queue
+            }
+            //Display Each players cards
+            foreach (var Print in players)
+            {
+                Console.WriteLine(Print + "\n");
+            }
+        }
     }
 }
