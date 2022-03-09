56060d70006e75334531a1cec74ce1be37272640
Tulasi <93372595+tula1100@users.noreply.github.com>
2022-03-09 22:46:45 +0530

    Update Stock.cs

--- Stock.cs
+++ Stock.cs
@@ -8,5 +8,14 @@ namespace Inventory_Problems_day_11_and_12
 {
     internal class Stock
     {
+       public String StockName;
+        public long NumberOfShare;
+        public long SharePrice;
+        public long TotalStockPrice;
+        public override string ToString()
+        {
+            string st = $"StockName: {StockName}\nNumberOfShare: {NumberOfShare}\nSharePrice: {SharePrice}\nTotalStockPrice: {TotalStockPrice}";
+            return st;
+        }
     }
 }
