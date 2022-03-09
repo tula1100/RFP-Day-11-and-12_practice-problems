d7fc4bbde55f63fe1e8e4aa4f0e17733d0257ec9
Tulasi <93372595+tula1100@users.noreply.github.com>
2022-03-09 22:38:42 +0530

    Update CommericialDataManagement.cs

--- CommericialDataManagement.cs
+++ CommericialDataManagement.cs
@@ -8,5 +8,72 @@ namespace Inventory_Problems_day_11_and_12
 {
     internal class CommericialDataManagement
     {
+        String FilePath = @"D:\.net\jason\Commmercial-Jason.json";
+        public List<StockAccount> account;
+        public CommericialDataManagement()
+        {
+            Selection();
+        }
+        public void Selection()
+        {
+            Console.WriteLine("1-->Add Stocks\n2-->Display Stocks\n3-->Remove Stocks");
+            int Select = int.Parse(Console.ReadLine());
+            switch (Select)
+            {
+                case 1: AddStock(); break;
+                case 2: Display(); break;
+                case 3: Remove(); break;
+                default: Environment.Exit(0); break;
+            }
+        }
+        public void AddStock()
+        {
+            Console.WriteLine("Enter the Name: ");
+            String Name = Console.ReadLine();
+            StockAccount ac = new StockAccount();
+            ac.Fill(Name);
+            if (account == null)
+            {
+                account = new List<StockAccount>();
+            }
+            string Read = File.ReadAllText(FilePath);
+            account.Add(ac);
+            string ClassInformation = JsonConvert.SerializeObject(account);
+            File.WriteAllText(FilePath, ClassInformation);
+            Selection();
+        }
+        public void Remove()
+        {
+            string Read = File.ReadAllText(FilePath);
+            if (account == null)
+            {
+                account = JsonConvert.DeserializeObject<List<StockAccount>>(Read);
+            }
+            Console.Write("Enter the Name ");
+            String name = Console.ReadLine();
+            StockAccount[] Copy = account.ToArray();
+            List<StockAccount> NewList = new List<StockAccount>();
+            for (int i = 0; i < Copy.Length; i++)
+            {
+                if (Copy[i].Name != name)
+                {
+                    NewList.Add(Copy[i]);
+                }
+            }
+            string serialize = JsonConvert.SerializeObject(NewList);
+            File.WriteAllText(FilePath, serialize);
+            Selection();
+        }
+        public void Display()
+        {
+            string Read = File.ReadAllText(FilePath);
+            List<StockAccount> list = new List<StockAccount>();
+            list = JsonConvert.DeserializeObject<List<StockAccount>>(Read);
+            foreach (var Print in list)
+            {
+                Print.PrintReport();
+            }
+            Selection();
+        }
     }
 }
